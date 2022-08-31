da1177e98001f8c3ffa0b73672b0aecd2e705ff1
ilona <gogiashvili.ilona@gmail.com>
2022-08-31 11:40:56 -0400

    commit tree eedd179dab28ea3a084073e2fdee342eb52b2e58

---
+++ index.js
@@ -0,0 +1,14 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
+function introduction(Name);
+console.log("Hi, my name is ${name}");
+
+
+
